94bfbc3ab89a6d608232ae466b9e8f42538a119e
Haritha Randunu <randunuharitha2001@gmail.com>
2024-12-01 18:43:38 +0530

    Modify Hello_Flutter_App into selection types

--- lib/joke_service.dart
+++ lib/joke_service.dart
@@ -1,20 +1,35 @@
-
+import 'dart:convert';
 
 import 'package:dio/dio.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
 class JokeService {
   final Dio _dio = Dio();
   final String baseUrl = 'https://v2.jokeapi.dev/joke';
 
-  Future<List<Map<String, dynamic>>> fetchJokesRaw() async {
+  Future<List<Map<String, dynamic>>?> fetchJokesRaw(
+      String? category, String? type, String? amount) async {
+    if (amount == '') {
+      return null;
+    }
     try {
-      final response = await _dio.get('$baseUrl/programming', 
-      queryParameters: {
-        'amount': 3,
-        'type': 'single,twopart',
-        'blacklistFlag': 'nsfw,religious,political,racist,sexist,explicit',
-      },
-      );
+      // final SharedPreferences cache = await SharedPreferences.getInstance();
+      final Response response;
+
+      if (amount == '1') {
+        response = await _dio.get('$baseUrl/$category', queryParameters: {
+          // 'amount': null,
+          'type': type,
+          // 'blacklistFlag': flags,
+        });
+      }
+      else {
+        response = await _dio.get('$baseUrl/$category', queryParameters: {
+          'amount': amount,
+          'type': type,
+          // 'blacklistFlag': flags,
+        });
+      }
 
       if (response.statusCode == 200) {
         if (response.data['error'] == true) {
@@ -22,14 +37,29 @@ class JokeService {
         }
 
         final List<dynamic> jokesJson = response.data['jokes'];
+        // await cache.setString('cachedJokes', jsonEncode(jokesJson));
         return jokesJson.cast<Map<String, dynamic>>();
-      }
-      else {
+      } else {
         throw Exception('Failed to load jokes: ${response.statusCode}');
       }
-    }
-    catch (e) {
-      throw Exception(e);
+    } catch (e) {
+      // return fetchFromCache();
+      throw Exception(e.toString());
     }
   }
+
+  // Future<List<Map<String, dynamic>>?> fetchFromCache() async {
+  //   final SharedPreferences cache = await SharedPreferences.getInstance();
+  //   final String? cachedJokes = cache.getString('cachedJokes');
+  //   if (cachedJokes != null) {
+  //     final List<dynamic> jokesJson = jsonDecode(cachedJokes);
+  //     return jokesJson.cast<Map<String, dynamic>>();
+  //   }
+  //   throw Exception("No cached jokes available for now...");
+  // }
+
+  // Future<bool> isConnected() async {
+  //   final ConnectivityResult result = (await Connectivity().checkConnectivity()) as ConnectivityResult;
+  //   return result == ConnectivityResult.mobile || result == ConnectivityResult.wifi;
+  // }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
+// import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'joke_service.dart';
-import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:hello_flutter_app/joke_service.dart';
 
-void main() => runApp(const MyApp());
+// void main() => runApp(const MyApp());
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+// void main() {
+//   runApp(
+//       DevicePreview(
+//           builder: (context) => MyApp()
+//       )
+//   );
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext buildContext) {
     return MaterialApp(
       title: 'Joke App',
       theme: ThemeData(primarySwatch: Colors.blue),
@@ -24,42 +43,91 @@ class JokeListPage extends StatefulWidget {
   _JokeListPageState createState() => _JokeListPageState();
 }
 
+class Value_And_ChildText {
+  late String value;
+  late String childText;
+
+  Value_And_ChildText(this.value, this.childText);
+}
+
 class _JokeListPageState extends State<JokeListPage> {
   final JokeService _jokeService = JokeService();
   List<Map<String, dynamic>> _jokesRaw = [];
   bool _isLoading = false;
 
+  final List<String> _dropdownCategories = [
+    'Any',
+    'Miscellaneous',
+    'Programming',
+    'Pun',
+    'Spooky',
+    'Christmas',
+  ];
+
+  final List<Value_And_ChildText> _dropDownTypes = [
+    Value_And_ChildText("any", "Any"),
+    Value_And_ChildText("single", "Single"),
+    Value_And_ChildText("twopart", "Two Part")
+  ];
+
+  final List<String> _noOfAmount = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
+  ];
+
+  String? _selectedCategory = 'Any';
+  String? _selectedType = 'any';
+  String? _selectedAmount = '10';
+
   Future<void> _fetchJokes() async {
+    // if (_selectedAmount == '') {
+    //   setState(() {
+    //     _jokesRaw = [];
+    //   });
+    //   throw Exception();
+    // }
     setState(() => _isLoading = true);
     try {
-      _jokesRaw = await _jokeService.fetchJokesRaw();
+      _jokesRaw = (await _jokeService.fetchJokesRaw(
+          _selectedCategory, _selectedType, _selectedAmount))!;
       setState(() => _isLoading = false);
+      if (_jokesRaw == []) {
+        throw Exception();
+      }
     } catch (e) {
-       throw Exception(e);
+      setState(() {
+        _jokesRaw = [];
+        _isLoading = false;
+      });
+      throw Exception('There is an error.');
     }
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext builderContext) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           'Joke App',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.white
-          ),
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
         ),
         backgroundColor: Colors.deepPurple,
       ),
       body: Container(
         decoration: BoxDecoration(
             gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Colors.deepPurple.shade100, Colors.white],
-            )
-        ),
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Colors.deepPurple.shade100, Colors.white],
+        )),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
@@ -71,8 +139,7 @@ class _JokeListPageState extends State<JokeListPage> {
                     fontSize: 28,
                     fontWeight: FontWeight.bold,
                     color: Colors.deepPurple,
-                    shadows: [Shadow(color: Colors.white, blurRadius: 2)]
-                ),
+                    shadows: [Shadow(color: Colors.white, blurRadius: 2)]),
                 textAlign: TextAlign.center,
               ),
               const SizedBox(height: 16),
@@ -86,15 +153,96 @@ class _JokeListPageState extends State<JokeListPage> {
                 ),
                 textAlign: TextAlign.center,
               ),
-              const SizedBox(height: 20),
+              const SizedBox(height: 5),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Type',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 15,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 110,
+                            child: DropdownButton(
+                              value: _selectedType,
+                              items: _dropDownTypes
+                                  .map((Value_And_ChildText item) {
+                                return DropdownMenuItem(
+                                  value: item.value,
+                                  child: Text(item.childText),
+                                );
+                              }).toList(),
+                              onChanged: (String? value) {
+                                setState(() {
+                                  _selectedType = value;
+                                });
+                              },
+                              dropdownColor: Colors.deepPurple[200],
+                              elevation: 30,
+                              padding: const EdgeInsets.all(10.0),
+                              borderRadius: BorderRadius.circular(12),
+                              style: TextStyle(
+                                  color: Colors.black87,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w700),
+                              underline: Container(
+                                height: 5,
+                                color: Colors.deepPurple[100],
+                              ),
+                              icon: const Icon(Icons.arrow_drop_down),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    DropdownButton(
+                      value: _selectedCategory,
+                      items: _dropdownCategories.map((String? item) {
+                        return DropdownMenuItem(
+                            value: item,
+                            child: Text(item!),
+                        );
+                      }).toList(),
+                      onChanged: (String? value) {
+                        setState(() {
+                          _selectedCategory = value;
+                        });
+                      },
+                    ),
+                    DropdownButton(
+                        value: _selectedAmount,
+                        items: _noOfAmount.map((String? item) {
+                          return DropdownMenuItem(
+                              value: item, child: Text(item!));
+                        }).toList(),
+                        onChanged: (String? value) {
+                          setState(() {
+                            _selectedAmount = value;
+                          });
+                        }),
+                  ],
+                ),
+              ),
               ElevatedButton(
-                  onPressed: () {_fetchJokes();},
+                  onPressed: () {
+                    _fetchJokes();
+                  },
                   style: const ButtonStyle(
-                    backgroundColor: WidgetStatePropertyAll<Color>(Colors.deepPurple),
-                    minimumSize: WidgetStatePropertyAll<Size>(Size(0, 50)),
-                    shape: WidgetStatePropertyAll(LinearBorder.none),
-                    enableFeedback: true
-                  ),
+                      backgroundColor:
+                          WidgetStatePropertyAll<Color>(Colors.deepPurple),
+                      minimumSize: WidgetStatePropertyAll<Size>(Size(0, 50)),
+                      shape: WidgetStatePropertyAll(LinearBorder.none),
+                      enableFeedback: true),
                   child: const Text(
                     'Fetch Jokes',
                     style: TextStyle(
@@ -105,8 +253,8 @@ class _JokeListPageState extends State<JokeListPage> {
                   )),
               const SizedBox(height: 24),
               Expanded(
-                child: _isLoading ?
-                const Center(child: CircularProgressIndicator())
+                child: _isLoading
+                    ? const Center(child: CircularProgressIndicator())
                     : _buildJokeList(),
               )
             ],
@@ -116,32 +264,71 @@ class _JokeListPageState extends State<JokeListPage> {
     );
   }
 
-
   Widget _buildJokeList() {
-    if (_jokesRaw.isEmpty) {
+    if (_selectedAmount!.isEmpty && _jokesRaw.isEmpty) {
       return const Center(
         child: Text(
-          'No jokes fetched yet.',
-          style: TextStyle(fontSize: 18, color: Colors.deepPurple),
+          'Required the amount of jokes you idiot..',
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.deepOrange,
+          ),
         ),
       );
+    }
+    if (_jokesRaw.isEmpty) {
+      return const Center(
+          child: Text(
+        'No jokes fetched yet.',
+        style: TextStyle(
+          fontSize: 18,
+          color: Colors.deepPurple,
+        ),
+      ));
     }
     return ListView.builder(
       itemCount: _jokesRaw.length,
       itemBuilder: (context, index) {
         final jokeJson = _jokesRaw[index];
-        return Card(
-          margin: const EdgeInsets.only(bottom: 16),
-          elevation: 4,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              jsonEncode(jokeJson),
-              style: const TextStyle(fontSize: 14),
-            )
-          ),
-        );
+        if ('${jokeJson['type']}' == 'single') {
+          return Card(
+            margin: const EdgeInsets.only(bottom: 16),
+            elevation: 10,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  '${jokeJson['joke']}',
+                  style: const TextStyle(fontSize: 14),
+                  textAlign: TextAlign.center,
+                )),
+          );
+        } else {
+          return Card(
+            margin: const EdgeInsets.only(bottom: 16),
+            elevation: 10,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Text(
+                    '${jokeJson['setup']}',
+                    style: const TextStyle(fontSize: 15),
+                    textAlign: TextAlign.left,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    '${jokeJson['delivery']}',
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
       },
     );
   }
